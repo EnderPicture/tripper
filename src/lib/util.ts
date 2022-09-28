a2ef8eab9d35bcc482027883c8d5bf8714667761
enderpicture <enderpicture@gmail.com>
2022-09-27 21:17:17 -0700

    fix: issue with non https uuid

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,12 @@
-
 export const uuid = () => {
-  return self.crypto.randomUUID();
-}
+	try {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+			var r = (Math.random() * 16) | 0,
+				v = c == 'x' ? r : (r & 0x3) | 0x8;
+			return v.toString(16);
+		});
+	} catch (e) {
+		// no crypto in non https
+		return Math.random() + Date.now() + '';
+	}
+};
